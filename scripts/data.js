b2f10501da6674b5ad1cead58cf31e6698d2a854
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-08-05 09:42:47 -0700

    add data.js

---
+++ scripts/data.js
@@ -0,0 +1,20 @@
+export const data = [
+  {
+    id: 0,
+    title: "Rick and Morty",
+    img: "https://bit.ua/wp-content/uploads/2018/05/social-1.jpg",
+    text: "An animated series on adult-swim about the infinite adventures of Rick, a genius alcoholic and careless scientist, with his grandson Morty, a 14 year-old anxious boy who is not so smart. Together, they explore the infinite universes; causing mayhem and running into trouble."
+  },
+  {
+    id: 1,
+    title: "Adventure time",
+    img: "https://is2-ssl.mzstatic.com/image/thumb/Music125/v4/65/79/ab/6579abc9-fa9f-cbd0-8adc-37f80408e2dc/859731656150.jpg/1200x1200bf-60.jpg",
+    text: " A boy, Finn the human, and his adopted adoptive brother, Jake the dog, live together and go on adventures in the land of Ooo, which is filled with odd creatures and beings. The land has many kingdoms, most of which are led by a monarch. Mostly, we see princesses, which are named for their physical form. The land of Ooo is on Earth, but the planet was wiped out 1000 years ago in the Great Mushroom War, leaving behind a mutated surface and the wreckage of our civilization."
+  },
+  {
+    id: 2,
+    title: "Gravity Falls",
+    img: "https://media.myshows.me/shows/normal/e/47/e47c3386832b8399ee0a8c8ccb0770d2.jpg",
+    text: "Dipper and Mabel Pines are mystery twins. They are in for when they are sent off to spend the summer with their great uncle in the mysterious town of Gravity Falls, Oregon. Upon their arrival, Dipper and Mabel's huckster great uncle, Grunkle Stan, enlists the siblings' help in running the Mystery Shack, a tourist trap he owns that overcharges unsuspecting customers. Although Dipper and Mabel quickly discover that the Mystery Shack itself is a hoax, they soon discover that there is something truly strange about their new town, and together they begin to unlock the real secrets of Gravity Falls."
+  },
+];
